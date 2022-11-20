92b1dd5d32b2d0ce9dad65002e32578c3cc270cd
ankitzin <ankit.zin279@gmail.com>
2022-11-20 12:20:25 +0530

    Static files added basic structure completed

--- notes/urls.py
+++ notes/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes', views.NotesListView.as_view()),
-    path('notes/<int:pk>', views.NoteDetailView.as_view()),
+    path('notes', views.NotesListView.as_view(), name="notes.list"),
+    path('notes/<int:pk>', views.NoteDetailView.as_view(), name="notes.detail"),
 ]
 
 # urlpatterns = [
