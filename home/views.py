1e91d955686064bc543d7b4b5002c5a33134fc40
ankitzin <ankit.zin279@gmail.com>
2022-11-25 00:11:13 +0530

    Finished this full with small project Adding notes in proper user.

--- home/views.py
+++ home/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'home/register.html'
+    success_url = '/smart/notes'
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('notes.list')
+        return super().get(request, *args, **kwargs)
 
 class LoginInterfaceView(LoginView):
     template_name = 'home/login.html'
